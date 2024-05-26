08a753034a1ca92e40e255901b6694d256b49be3
HFasnacht577 <160857259+HFasnacht577@users.noreply.github.com>
2024-05-26 16:11:27 -0400

    Add files via upload

---
+++ WebScraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib,ssl,pushbullet
+
+r = requests.get('https://www.playstation.com/en-us/games/hades/')
+
+
+soup = BeautifulSoup(r.content,'html.parser')
+
+s = soup.find('span',class_="psw-t-title-m").get_text()
+
+s_clean = float(s.strip('$'))
+
+if s_clean < 24.99:
+    pb = pushbullet.PushBullet('o.52djiptcTTkImnymIHi34GjStcSUsdYx')
+    pb.push_note('Sale Detected','Sale on Hades detected: ' + s)
