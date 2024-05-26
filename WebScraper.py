5f068207de10e3560e8e987a4c5f27a2db677572
HFasnacht577 <160857259+HFasnacht577@users.noreply.github.com>
2024-05-26 17:31:33 -0400

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -12,5 +12,5 @@ s = soup.find('span',class_="psw-t-title-m").get_text()
 s_clean = float(s.strip('$'))
 
 if s_clean == 24.99:
-    pb = pushbullet.PushBullet(PushBullet_API)
+    pb = pushbullet.PushBullet(os.environ['PUSHBULLET_API'])
     pb.push_note('Sale Detected','Sale on Hades detected: ' + s)
