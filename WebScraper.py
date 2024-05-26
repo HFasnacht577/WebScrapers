69d108d8623a04cfe84a2278757ffeb76604d5c4
HFasnacht577 <160857259+HFasnacht577@users.noreply.github.com>
2024-05-26 19:01:50 -0400

    Update WebScraper.py
    Added Error handling and secret key access

--- WebScraper.py
+++ WebScraper.py
@@ -1,16 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-import smtplib,ssl,pushbullet
+import smtplib,ssl,pushbullet,os
 
-r = requests.get('https://www.playstation.com/en-us/games/hades/')
-
+pb = pushbullet.PushBullet(os.environ.get('SECRET_KEY')
+try:
+    r = requests.get('https://www.playstation.com/en-us/games/hades/')
+except:
+    pb.push_note('Action Error','Unable to access game listing')
+    return
 
 soup = BeautifulSoup(r.content,'html.parser')
+try:
+    s = soup.find('span',class_="psw-t-title-m").get_text()
+    s_clean = float(s.strip('$'))
+except:
+    pb.push_note('Action Error','Error retrieving and processing price data')
+    return
 
-s = soup.find('span',class_="psw-t-title-m").get_text()
-
-s_clean = float(s.strip('$'))
-
-if s_clean == 24.99:
-    pb = pushbullet.PushBullet($secret_key)
-    pb.push_note('Sale Detected','Sale on Hades detected: ' + s)
+try:
+    if s_clean == 24.99:
+        pb.push_note('Sale Detected','Sale on Hades detected: ' + s)
+        return
+except:
+    pb.push_note('Action Error','Error with sale detection submission.')
+    return
