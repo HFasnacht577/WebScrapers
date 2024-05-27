c2d567ffcb320e84ec1f5d3065e1928fa944e44b
HFasnacht577 <hunterfasnacht577@gmail.com>
2024-05-27 15:10:29 -0400

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -1,26 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
-import smtplib,ssl,pushbullet,os
+import smtplib,ssl,pushbullet,os,datetime
 
-pb = pushbullet.PushBullet(os.environ.get('SECRET_KEY'))
-try:
-    r = requests.get('https://www.playstation.com/en-us/games/hades/')
-except:
-    pb.push_note('Action Error','Unable to access game listing')
-    quit()
+with open('runlog.txt','a') as f:
+    current_time = str(datetime.now())
 
-soup = BeautifulSoup(r.content,'html.parser')
-try:
-    s = soup.find('span',class_="psw-t-title-m").get_text()
-    s_clean = float(s.strip('$'))
-except:
-    pb.push_note('Action Error','Error retrieving and processing price data')
-    quit()
+    pb = pushbullet.PushBullet(os.environ.get('SECRET_KEY'))
 
-try:
-    if s_clean < 24.99:
-        pb.push_note('Sale Detected','Sale on Hades detected: ' + s)
+    try:
+        r = requests.get('https://www.playstation.com/en-us/games/hades/')
+    except Exception as e:
+        pb.push_note('Action Error','Unable to access game listing')
+        f.write('Run Failed: ' + current_time +  ' Error: ' + str(e) + '\n')
+        f.flush()
         quit()
-except:
-    pb.push_note('Action Error','Error with sale detection submission.')
-    quit()
+
+    soup = BeautifulSoup(r.content,'html.parser')
+    try:
+        price = soup.find('span',class_="psw-t-title-m").get_text()
+        num_price = float(price.strip('$'))
+    except Exception as e:
+        pb.push_note('Action Error','Error processing price data')
+        f.write('Run Failed: ' + current_time +  ' Error: ' + str(e) + '\n')
+        f.flush()
+        quit()
+
+    try:
+        if num_price < 24.99:
+            pb.push_note('Sale Detected','Sale on Hades detected: ' + price)
+    except Exception as e:
+        pb.push_note('Action Error','Error with price comparison')
+        f.write('Run Failed: ' + current_time +  ' Error: ' + str(e) + '\n')
+        f.flush()
+        quit()
+
+    try:
+        f.write('Run Successful: ' + current_time + ' Price: ' + price + '\n')
+        f.flush()
+    except:
+        pb.push_note('Action Error','Unable to write to runlog')
