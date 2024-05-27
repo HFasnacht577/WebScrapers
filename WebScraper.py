d73ebb129dd4cc671556e848f618959bf80c286c
HFasnacht577 <hunterfasnacht577@gmail.com>
2024-05-27 16:00:34 -0400

    change structure for simpler running

--- WebScraper.py
+++ WebScraper.py
@@ -3,39 +3,35 @@ from bs4 import BeautifulSoup
 import smtplib,ssl,pushbullet,os,datetime
 
 with open('runlog.txt','a') as f:
+
     current_time = str(datetime.datetime.now())
 
     pb = pushbullet.PushBullet(os.environ.get('SECRET_KEY'))
 
-    try:
-        r = requests.get('https://www.playstation.com/en-us/games/hades/')
-    except Exception as e:
-        pb.push_note('Action Error','Unable to access game listing')
-        f.write('Run Failed: ' + current_time +  ' Error: ' + str(e) + '\n')
+    def log_and_notify(e,current_time=current_time,pb=pb,f=f):
+        pb.push_note('Action Error',f'Error: {str(e)}')
+        f.write(f'Run Failed: {current_time} Error: {str(e)}\n')
         f.flush()
-        quit()
 
-    soup = BeautifulSoup(r.content,'html.parser')
+
     try:
+        #Get website data
+        r = requests.get('https://www.playstation.com/en-us/games/hades/')
+
+        #Initialize soup
+        soup = BeautifulSoup(r.content,'html.parser')
+
+        #get the price as text and float
         price = soup.find('span',class_="psw-t-title-m").get_text()
         num_price = float(price.strip('$'))
-    except Exception as e:
-        pb.push_note('Action Error','Error processing price data')
-        f.write('Run Failed: ' + current_time +  ' Error: ' + str(e) + '\n')
-        f.flush()
-        quit()
 
-    try:
+        #compare price to usual price, if cheaper than usual notify me through pushbullet
         if num_price < 24.99:
             pb.push_note('Sale Detected','Sale on Hades detected: ' + price)
-    except Exception as e:
-        pb.push_note('Action Error','Error with price comparison')
-        f.write('Run Failed: ' + current_time +  ' Error: ' + str(e) + '\n')
-        f.flush()
-        quit()
 
-    try:
+        #if succesful add to runlog
         f.write('Run Successful: ' + current_time + ' Price: ' + price + '\n')
         f.flush()
-    except:
-        pb.push_note('Action Error','Unable to write to runlog')
+
+    except Exception as e:
+        log_and_notify(e)
