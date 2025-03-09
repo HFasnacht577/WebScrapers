5e4bde7779fa5d23706f68b7a4fd93e5c2b65a40
HFasnacht577 <160857259+HFasnacht577@users.noreply.github.com>
2025-03-08 23:33:00 -0500

    Changed to match bloodborne

--- WebScraper.py
+++ WebScraper.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib,ssl,pushbullet,os,datetime
 
+web_link = 'https://store.playstation.com/en-us/product/UP9000-CUSA00900_00-SPEXPANSIONDLC03'
+max_price = 19.99
+
 with open('runlog.txt','a') as f:
 
     current_time = str(datetime.datetime.now())
@@ -16,7 +19,7 @@ with open('runlog.txt','a') as f:
 
     try:
         #Get website data
-        r = requests.get('https://www.playstation.com/en-us/games/hades/')
+        r = requests.get(web_link)
 
         #Initialize soup
         soup = BeautifulSoup(r.content,'html.parser')
@@ -26,7 +29,7 @@ with open('runlog.txt','a') as f:
         num_price = float(price.strip('$'))
 
         #compare price to usual price, if cheaper than usual notify me through pushbullet
-        if num_price < 24.99:
+        if num_price < max_price:
             pb.push_note('Sale Detected','Sale on Hades detected: ' + price)
 
         #if succesful add to runlog
